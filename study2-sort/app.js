248ba9c5843dfd2321c64674cbe27fdb86ebff75
seki <schokolade0317@gmail.com>
2018-06-17 18:02:28 +0900

    enable create random array

--- study2-sort/app.js
+++ study2-sort/app.js
@@ -4,7 +4,7 @@ class Book {
         this.price = price;
     }
 }
-let books = new Array(new Book("test3",100),new Book("test4",200),new Book("test2",50),new Book("test1",600))
+let books = createRandomBooks()
 
 $(window).load(function () {
     display()
@@ -14,4 +14,16 @@ function display() {
     books.forEach(function (book) {
         $("#book-table").append(`<tr><td>${book.title}</td><td>${book.price}</td></tr>`)
     })
+}
+
+function createRandomBooks() {
+    let books = [];
+    for(let i = 0;i < 100;i++){
+        books.push(new Book("book" + i,getRandomInt(100,10000)));
+    }
+    return books;
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
